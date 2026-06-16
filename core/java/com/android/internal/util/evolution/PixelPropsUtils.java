6f4a51f6faad3982d404c849cba82074447fbefd
Joey <joey@evolution-x.org>
2026-06-17 03:24:24 +0900

    PixelPropsUtils: Update fingerprints to June 2026 release

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -260,8 +260,8 @@ public final class PixelPropsUtils {
         propsToChangeRecentPixel.put("PRODUCT", "mustang");
         propsToChangeRecentPixel.put("HARDWARE", "mustang");
         propsToChangeRecentPixel.put("MODEL", "Pixel 10 Pro XL");
-        propsToChangeRecentPixel.put("ID", "CP1A.260505.005");
-        propsToChangeRecentPixel.put("FINGERPRINT", "google/mustang/mustang:16/CP1A.260505.005/15081906:user/release-keys");
+        propsToChangeRecentPixel.put("ID", "CP2A.260605.012");
+        propsToChangeRecentPixel.put("FINGERPRINT", "google/mustang/mustang:17/CP2A.260605.012/15430684:user/release-keys");
         propsToChangePixelTablet = new HashMap<>();
         propsToChangePixelTablet.put("BRAND", "google");
         propsToChangePixelTablet.put("BOARD", "tangorpro");
@@ -270,8 +270,8 @@ public final class PixelPropsUtils {
         propsToChangePixelTablet.put("PRODUCT", "tangorpro");
         propsToChangePixelTablet.put("HARDWARE", "tangorpro");
         propsToChangePixelTablet.put("MODEL", "Pixel Tablet");
-        propsToChangePixelTablet.put("ID", "CP1A.260505.005");
-        propsToChangePixelTablet.put("FINGERPRINT", "google/tangorpro/tangorpro:16/CP1A.260505.005/15081906:user/release-keys");
+        propsToChangePixelTablet.put("ID", "CP2A.260605.012");
+        propsToChangePixelTablet.put("FINGERPRINT", "google/tangorpro/tangorpro:17/CP2A.260605.012/15430684:user/release-keys");
         propsToChangeMeizu = new HashMap<>();
         propsToChangeMeizu.put("BRAND", "meizu");
         propsToChangeMeizu.put("MANUFACTURER", "Meizu");
